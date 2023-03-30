aa4aa428e1f1d575e6ed7c84a6ad7b87273c3621
KYUTAE PARK <rbxo0109@gmail.com>
2023-03-31 01:01:28 +0900

    feat: page 설정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import MainPage from './page/MainPage';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <MainPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
