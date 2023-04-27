61848b98586137c33c56a2597b3bcbaa0462e591
KYUTAE PARK <rbxo0109@gmail.com>
2023-04-28 01:17:52 +0900

    feat: Result Page css 정리

--- my-app/src/error/ErrorBoundary.js
+++ my-app/src/error/ErrorBoundary.js
@@ -12,20 +12,20 @@ class ErrorBoundary extends React.Component{
         // 렌더링 할 때 fallback UI가 보이도록 하는것
         return {
             hashError:true
-        };
+        }
     }
     componentDidCatch(error,errorInfo){
         //erroe를 서버로 처리하는 방법
         console.error(error,errorInfo);
     }
 
-    reder(){
-        const {fallback, childeren}  = this.props
+    render(){
+        const {fallback, children}  = this.props
         if (this.state.hashError){
             //별도로 작성할 대체 UI
             return fallback;
         }
-        return childeren;
+        return children;
     }
 
 }
